fd294b4d25bd653bd4cd391c4a97c50ed6e59bfd
seitamuro <seitamuro0210@gmail.com>
2024-03-17 11:10:25 +0900

    feat(server): 登録時に重複する本が無いかを確認するようにした

--- src/server/graphql/mutation_resolver.ts
+++ src/server/graphql/mutation_resolver.ts
@@ -12,10 +12,14 @@ type MutationResolver = {
 
 export const mutationResolver: MutationResolver = {
   createBook: async (_parent, _args, _context, _info) => {
-    const book = dataSource.manager.create(Book, {
-      title: _args.title,
-      author: _args.author,
-      price: _args.price,
+    const book: Book = await dataSource.transaction(async (manager) => {
+      const exists = await manager.findOne(Book, {
+        where: { title: _args.title },
+      });
+      if (exists) {
+        return exists;
+      }
+      return book;
     });
     await dataSource.manager.save(book);
     return book;
